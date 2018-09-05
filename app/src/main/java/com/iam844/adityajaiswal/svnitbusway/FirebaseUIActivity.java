56ce2c233766f67e6c38ca71fe1427e852cb44dd
iam844 <jaiswaladitya844@gmail.com>
2018-09-06 03:27:29 +0530

    Initial commit

---
+++ app/src/main/java/com/iam844/adityajaiswal/svnitbusway/FirebaseUIActivity.java
@@ -0,0 +1,32 @@
+package com.iam844.adityajaiswal.svnitbusway;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FirebaseUIActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_ui);
+    }
+
+
+
+
+}
