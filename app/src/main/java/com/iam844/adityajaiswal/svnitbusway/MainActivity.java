9ee8dbeb7399a52cb2529fbe3cfce537489a19e9
iam844 <jaiswaladitya844@gmail.com>
2018-09-06 21:55:53 +0530

    Edits

--- app/src/main/java/com/iam844/adityajaiswal/svnitbusway/MainActivity.java
+++ app/src/main/java/com/iam844/adityajaiswal/svnitbusway/MainActivity.java
@@ -1,20 +1,8 @@
 package com.iam844.adityajaiswal.svnitbusway;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
